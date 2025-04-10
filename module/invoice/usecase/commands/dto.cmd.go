d4f601bc469ce201f635b82749dcf33b20e7ca94
unknown <blessforwork@gmail.com>
2025-04-10 12:54:37 +0700

    remove Unnecessary dto

--- module/invoice/usecase/commands/dto.cmd.go
+++ module/invoice/usecase/commands/dto.cmd.go
@@ -1,13 +1,1 @@
 package invoicecommands
-
-import "github.com/google/uuid"
-
-type DetailInvoiceCmdDTO struct {
-	Id        uuid.UUID `json:"id"`
-	OrderCode int64     `json:"order-code"`
-	TotalFee  float64   `json:"total-fee"`
-}
-
-type UrlPayment struct {
-	Url string `json:"url"`
-}
