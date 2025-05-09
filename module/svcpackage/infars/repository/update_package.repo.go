649b844a7647a9b7f071b956a8953c1fbe8f08b3
PhuPhuoc <blessforwork@gmail.com>
2025-05-09 16:38:14 +0700

    update api update service-package, service-task

--- module/svcpackage/infars/repository/update_package.repo.go
+++ module/svcpackage/infars/repository/update_package.repo.go
@@ -3,9 +3,16 @@ package svcpackagerepository
 import (
 	"context"
 
+	"github.com/PhuPhuoc/curanest-appointment-service/common"
 	svcpackagedomain "github.com/PhuPhuoc/curanest-appointment-service/module/svcpackage/domain"
 )
 
 func (repo *svcPackageRepo) UpdatePackage(ctx context.Context, entity *svcpackagedomain.ServicePackage) error {
-	return nil
+	dto := ToSvcPackageDTO(entity)
+	where := "id=:id"
+	query := common.GenerateSQLQueries(common.UPDATE, TABLE_PACKAGE, UPDATE_FIELD_PACKAGE, &where)
+
+	// If no transaction, use db directly
+	_, err := repo.db.NamedExec(query, dto)
+	return err
 }
--- module/svcpackage/infars/repository/update_task.repo.go
+++ module/svcpackage/infars/repository/update_task.repo.go
@@ -3,9 +3,16 @@ package svcpackagerepository
 import (
 	"context"
 
+	"github.com/PhuPhuoc/curanest-appointment-service/common"
 	svcpackagedomain "github.com/PhuPhuoc/curanest-appointment-service/module/svcpackage/domain"
 )
 
 func (repo *svcPackageRepo) UpdateTask(ctx context.Context, entity *svcpackagedomain.ServiceTask) error {
-	return nil
+	dto := ToSvcTaskDTO(entity)
+	where := "id=:id"
+	query := common.GenerateSQLQueries(common.UPDATE, TABLE_TASK, UPDATE_FIELD_TASK, &where)
+
+	// If no transaction, use db directly
+	_, err := repo.db.NamedExec(query, dto)
+	return err
 }
